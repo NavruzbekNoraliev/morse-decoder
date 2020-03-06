3b168c21b04ceae4f123d9ddde621b62a5901fba
Navruzbek <navruzbek.noraliev@gmail.com>
2020-03-06 18:31:46 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -36,9 +36,21 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+// . stands for 10
+// - stands for 11 
 function decode(expr) {
-    // write your solution here
+    segmentedArg = expr.match(/.{1,10}/g)
+    let decodedMessage = ''
+    segmentedArg.forEach(element => {
+        if(element === '**********') {
+            decodedMessage += ' ';
+        } else {
+            const newSegement =  (parseInt(element)).toString().match(/.{1,2}/g)
+            const sign = newSegement.map(item => item === '10' ? '.' : '-').join('')
+            decodedMessage += MORSE_TABLE[sign]
+        }
+    });
+    return decodedMessage
 }
 
 module.exports = {
